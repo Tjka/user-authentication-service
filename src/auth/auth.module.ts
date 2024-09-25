32e91dbaa89a64b6c960709b5221b10177faa174
Matias Tika <matiastika@Matiass-MacBook-Pro.local>
2024-09-25 20:25:46 -0300

    initial commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { jwtConstants } from './constants';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true, // We're registering the JwtModule as global to make things easier for us. This means that we don't need to import the JwtModule anywhere else in our application.
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' }, // Short-lived
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    AuthService,
+  ],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
+
